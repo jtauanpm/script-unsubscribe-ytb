765beb823c10514dbab98854f2103664d278f895
Jonathan Tauan Pereira Maia <jtauan10@gmail.com>
2021-04-29 14:31:51 -0300

    Create bot.py
    Abrindo navegador

---
+++ bot.py
@@ -0,0 +1,14 @@
+import pyautogui
+import pyperclip
+import time
+pyautogui.PAUSE = 0.5
+# Abrir navegador (Microsoft Edge)
+pyautogui.press("winleft")
+pyautogui.write("Edge")
+pyautogui.press("Enter")
+# Entrar no yotube
+# Ir em inscrições
+# Gerenciar
+# Clicar em Inscrito
+# Cancelar inscrição
+# Repetir processo
